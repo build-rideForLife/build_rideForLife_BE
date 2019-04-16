d5e6cd1989dfe69c79ddffa7ea045bf93d8f3dbb
Max Trestman <mtrestman1@Maxs-MacBook-Pro.local>
2019-04-16 11:21:17 -0400

    Added to READMe and added driver and rider endpoints

--- api/routes/user/userRoute.js
+++ api/routes/user/userRoute.js
@@ -2,29 +2,14 @@ const express = require('express');
 const router = express.Router();
 const db = require('../../../data/dbConfig')
 const restrict = require('../auth/restricted-middleware')
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const secret = require('../../secrets.js').jwtSecret;
+// const bcrypt = require('bcryptjs');
+// const jwt = require('jsonwebtoken');
+// const secret = require('../../secrets.js').jwtSecret;
  
 
 
-// get by id
-// router.get('/', (req, res) => {
-//     db('riders')
-//     .select('rider_id', 'username')
-//     .where({id})
-//     .first()
-//     .then(rider => {
-//         res.status(200).json(rider)
-//     })
-//     .catch(err => {
-//         res.json(err)
-//     })
-// })
-
-
 // TESTING TO SEE IF AUTH WORKS
-router.get('/', restrict, (req, res) => {
+router.get('/',  (req, res) => {
     db('riders')
     .then(users => {
         res.status(200).json(users)
@@ -34,12 +19,59 @@ router.get('/', restrict, (req, res) => {
     })
 })
 
+// get by id
+router.get('/:id', restrict, (req, res) => {
+    db('riders')
+    .select('riders_id', 'username', 'phone')
+    .where({ riders_id: req.params.id})
+    .first()
+    .then(rider => {
+        res.status(200).json(rider)
+    })
+    .catch(err => {
+        res.json(err)
+    })
+})
 
 
 // update for phone or whatever
+router.put('/:id', restrict, (req, res) => {
+    db('riders')
+    .where({ riders_id: req.params.id})
+    .update(req.body)
+    .then(count => {
+        if(count > 0) {
+            res.status(202).json({message: 'Update Successful'})
+        } else {
+            res.status(404).json({ message: 'record not found'})
+        }
+        
+    })
+    .catch(error => {
+        res.status(500).json({ message: `there was an error updating ${error}`})
+    })
+})
 
 
 
 // delete acct if they want to 
+router.delete('/:id', restrict, (req, res) => {
+    db('riders')
+    .where({ riders_id: req.params.id })
+    .del()
+    .then(count => {
+        if(count > 0) {
+            res.status(202).json({message: 'User account removed successfully'})
+        } else {
+            res.status(404).json({ message: 'there was an error deleting'})
+        }
+    })
+    .catch(error => {
+        res.status(500).json({ message: `there was an error deleting ${error}`})
+    })
+})
+
+
+
 
 module.exports = router;
